f18605fe771db4eaf3db53868519933d583f2725
Niu Liu <liuniu@tsinghua.org.cn>
2025-08-12 09:57:08 +1200

    add support for gpt-5 serials

--- src/main/java/org/neo/servaaibase/impl/OpenAIImpl.java
+++ src/main/java/org/neo/servaaibase/impl/OpenAIImpl.java
@@ -28,6 +28,12 @@ public class OpenAIImpl extends AbsOpenAIImpl {
         return new OpenAIImpl(inputDBConnection);
     }
 
+    public static String gpt_5 = "gpt-5";
+    public static String gpt_5_mini = "gpt-5-mini";
+    public static String gpt_5_nano = "gpt-5-nano";
+    public static String gpt_5_chat = "gpt-5-chat-latest";
+    public static String gpt_oss_120b = "gpt-oss-120b";
+    public static String gpt_oss_20b = "gpt-oss-20b";
     public static String gpt_41 = "gpt-4.1";
     public static String gpt_41_mini = "gpt-4.1-mini";
     public static String gpt_41_nano = "gpt-4.1-nano";
@@ -35,7 +41,6 @@ public class OpenAIImpl extends AbsOpenAIImpl {
     public static String o3 = "o3";
     public static String o3_mini = "o3-mini";
     public static String o1 = "o1";
-    public static String o1_preview = "o1-preview";
     public static String o1_mini = "o1-mini";
     public static String gpt_4o_mini = "gpt-4o-mini";
     public static String gpt_4o_mini_tts = "gpt-4o-mini-tts";
@@ -65,14 +70,20 @@ public class OpenAIImpl extends AbsOpenAIImpl {
     private Map<String, Boolean> supportSystemMapping;
 
     private void setup() {
-        chatModels = new String[]{gpt_41, gpt_41_mini, gpt_41_nano, o4_mini, o3, o3_mini, o1, o1_preview, o1_mini, gpt_4o_mini, gpt_4o, gpt_4_turbo_preview, gpt_35_turbo};
+        chatModels = new String[]{gpt_5, gpt_5_mini, gpt_5_nano, gpt_5_chat, gpt_41, gpt_41_mini, gpt_41_nano, o4_mini, o3, o3_mini, o1, o1_mini, gpt_4o_mini, gpt_4o, gpt_4_turbo_preview, gpt_35_turbo};
         embeddingModels = new String[]{text_embedding_3_large, text_embedding_3_small};
         imageModels = new String[]{dall_e_3, dall_e_2};
-        visionModels = new String[]{o3, o1, gpt_4o_mini, gpt_4o};
+        visionModels = new String[]{gpt_5, gpt_5_mini, gpt_5_nano, gpt_5_chat, o3, o1, gpt_4o_mini, gpt_4o};
         textToSpeechModels = new String[]{gpt_4o_mini_tts, tts_1_hd, tts_1};
         speechToTextModels = new String[]{gpt_4o_transcribe, gpt_4o_mini_transcribe, whisper_1};
 
         urlMapping = new ConcurrentHashMap<String, String>();
+        urlMapping.put(gpt_5, "https://api.openai.com/v1/chat/completions");
+        urlMapping.put(gpt_5_mini, "https://api.openai.com/v1/chat/completions");
+        urlMapping.put(gpt_5_nano, "https://api.openai.com/v1/chat/completions");
+        urlMapping.put(gpt_5_chat, "https://api.openai.com/v1/chat/completions");
+        urlMapping.put(gpt_oss_120b, "https://api.openai.com/v1/chat/completions");
+        urlMapping.put(gpt_oss_20b, "https://api.openai.com/v1/chat/completions");
         urlMapping.put(gpt_41, "https://api.openai.com/v1/chat/completions");
         urlMapping.put(gpt_41_mini, "https://api.openai.com/v1/chat/completions");
         urlMapping.put(gpt_41_nano, "https://api.openai.com/v1/chat/completions");
@@ -80,7 +91,6 @@ public class OpenAIImpl extends AbsOpenAIImpl {
         urlMapping.put(o3, "https://api.openai.com/v1/chat/completions");
         urlMapping.put(o3_mini, "https://api.openai.com/v1/chat/completions");
         urlMapping.put(o1, "https://api.openai.com/v1/chat/completions");
-        urlMapping.put(o1_preview, "https://api.openai.com/v1/chat/completions");
         urlMapping.put(o1_mini, "https://api.openai.com/v1/chat/completions");
         urlMapping.put(gpt_4o_mini, "https://api.openai.com/v1/chat/completions");
         urlMapping.put(gpt_4o, "https://api.openai.com/v1/chat/completions");
@@ -98,6 +108,12 @@ public class OpenAIImpl extends AbsOpenAIImpl {
         urlMapping.put(whisper_1, "https://api.openai.com/v1/audio/transcriptions");
 
         contextWindowMapping = new ConcurrentHashMap<String, Integer>();
+        contextWindowMapping.put(gpt_5, 400000);
+        contextWindowMapping.put(gpt_5_mini, 400000);
+        contextWindowMapping.put(gpt_5_nano, 400000);
+        contextWindowMapping.put(gpt_5_chat, 400000);
+        contextWindowMapping.put(gpt_oss_120b, 131072);
+        contextWindowMapping.put(gpt_oss_20b, 131072);
         contextWindowMapping.put(gpt_41, 1047576);
         contextWindowMapping.put(gpt_41_mini, 1047576);
         contextWindowMapping.put(gpt_41_nano, 1047576);
@@ -105,7 +121,6 @@ public class OpenAIImpl extends AbsOpenAIImpl {
         contextWindowMapping.put(o3, 200000);
         contextWindowMapping.put(o3_mini, 200000);
         contextWindowMapping.put(o1, 200000);
-        contextWindowMapping.put(o1_preview, 128000);
         contextWindowMapping.put(o1_mini, 128000);
         contextWindowMapping.put(gpt_4o_mini, 128000);
         contextWindowMapping.put(gpt_4o, 128000);
@@ -113,6 +128,12 @@ public class OpenAIImpl extends AbsOpenAIImpl {
         contextWindowMapping.put(gpt_35_turbo, 16385);
 
         maxOutputMapping = new ConcurrentHashMap<String, Integer>();
+        maxOutputMapping.put(gpt_5, 128000);
+        maxOutputMapping.put(gpt_5_mini, 128000);
+        maxOutputMapping.put(gpt_5_nano, 128000);
+        maxOutputMapping.put(gpt_5_chat, 128000);
+        maxOutputMapping.put(gpt_oss_120b, 131072);
+        maxOutputMapping.put(gpt_oss_20b, 131072);
         maxOutputMapping.put(gpt_41, 32768);
         maxOutputMapping.put(gpt_41_mini, 32768);
         maxOutputMapping.put(gpt_41_nano, 32768);
@@ -120,7 +141,6 @@ public class OpenAIImpl extends AbsOpenAIImpl {
         maxOutputMapping.put(o3, 100000);
         maxOutputMapping.put(o3_mini, 100000);
         maxOutputMapping.put(o1, 100000);
-        maxOutputMapping.put(o1_preview, 32768);
         maxOutputMapping.put(o1_mini, 65536);
         maxOutputMapping.put(gpt_4o_mini, 16384);
         maxOutputMapping.put(gpt_4o, 16384);
@@ -128,6 +148,12 @@ public class OpenAIImpl extends AbsOpenAIImpl {
         maxOutputMapping.put(gpt_35_turbo, 4096);
 
         supportSystemMapping = new ConcurrentHashMap<String, Boolean>();
+        supportSystemMapping.put(gpt_5, true);
+        supportSystemMapping.put(gpt_5_mini, true);
+        supportSystemMapping.put(gpt_5_nano, true);
+        supportSystemMapping.put(gpt_5_chat, true);
+        supportSystemMapping.put(gpt_oss_120b, true);
+        supportSystemMapping.put(gpt_oss_20b, true);
         supportSystemMapping.put(gpt_41, true);
         supportSystemMapping.put(gpt_41_mini, true);
         supportSystemMapping.put(gpt_41_nano, true);
@@ -135,7 +161,6 @@ public class OpenAIImpl extends AbsOpenAIImpl {
         supportSystemMapping.put(o3, true);
         supportSystemMapping.put(o3_mini, true);
         supportSystemMapping.put(o1, true);
-        supportSystemMapping.put(o1_preview, false);
         supportSystemMapping.put(o1_mini, false);
         supportSystemMapping.put(gpt_4o_mini, true);
         supportSystemMapping.put(gpt_4o, true);
